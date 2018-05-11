8dce93d193c4c39418d8ee928c2a593a6b67b3b5
Zhang <zchning@gmail.com>
2018-05-11 14:43:47 -0400

    add comments to public methods

--- src/main/java/io/xocore/kafka/StreamFilter.java
+++ src/main/java/io/xocore/kafka/StreamFilter.java
@@ -1,5 +1,15 @@
 package io.xocore.kafka;
 
+/**
+ * Define the interface to create a stream filter
+ */
 public interface StreamFilter {
+    /**
+     * Entry to filter the message
+     * @param topic Topic to be consumed
+     * @param key
+     * @param value Kafka message
+     * @return Boolean to decide if or not to process the message
+     */
     boolean run(String topic, Object key, Object value);
 }
